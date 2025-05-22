8fbed633011051b68dc7be2ce55c5de2ec909259
garvit1408 <garvitsinghgit@gmail.com>
2025-05-23 04:02:05 +0530

    All api route working

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
-from flask import Flask, jsonify, render_template, request
+from flask import Flask, jsonify, request
 import ipl
+import jugaad
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return "hello world"
 
 
 @app.route("/api/teams")
@@ -15,11 +16,34 @@ def teams():
     return jsonify(teams)
 
 
-@app.route("/api/teamVteam")
-def teamVteamAPI():
+@app.route("/api/teamvteam")
+def teamvteam():
     team1 = request.args.get("team1")
     team2 = request.args.get("team2")
+
     response = ipl.teamVteamAPI(team1, team2)
+    print(response)
+    return jsonify(response)
+
+
+@app.route("/api/team-record")
+def team_record():
+    team_name = request.args.get("team")
+    response = jugaad.teamAPI(team_name)
+    return response
+
+
+@app.route("/api/batting-record")
+def batting_record():
+    batsman_name = request.args.get("batsman")
+    response = jugaad.batsmanAPI(batsman_name)
+    return response
+
+
+@app.route("/api/bowling-record")
+def bowling_record():
+    bowler_name = request.args.get("bowler")
+    response = jugaad.bowlerAPI(bowler_name)
     return response
 
 
--- ipl.py
+++ ipl.py
@@ -1,36 +1,41 @@
-# Data Analysis Part
-
 import pandas as pd
 import numpy as np
 
 ipl_matches = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRy2DUdUbaKx_Co9F0FSnIlyS-8kp4aKv_I0-qzNeghiZHAI_hw94gKG22XTxNJHMFnFVKsO4xWOdIs/pub?gid=1655759976&single=true&output=csv"
 matches = pd.read_csv(ipl_matches)
 
-# print(matches.head())
+print(matches.head())
 
 
-# use colab for data analysis purpose, then simply copy-paste, colab is way more good
 def teamsAPI():
     teams = list(set(list(matches["Team1"]) + list(matches["Team2"])))
     team_dict = {"teams": teams}
+
     return team_dict
 
 
 def teamVteamAPI(team1, team2):
-    temp_df = matches[
-        (matches["Team1"] == team1) & (matches["Team2"] == team2)
-        | (matches["Team1"] == team2) & (matches["Team2"] == team1)
-    ]
-    total_matches = temp_df.shape[0]
-    matches_won_team1 = temp_df["WinningTeam"].value_counts()[team1]
-    matches_won_team2 = temp_df["WinningTeam"].value_counts()[team2]
+    valid_teams = list(set(list(matches["Team1"]) + list(matches["Team2"])))
 
-    draws = total_matches - (matches_won_team1 + matches_won_team2)
+    if team1 in valid_teams and team2 in valid_teams:
+        temp_df = matches[
+            (matches["Team1"] == team1) & (matches["Team2"] == team2)
+            | (matches["Team1"] == team2) & (matches["Team2"] == team1)
+        ]
+        total_matches = temp_df.shape[0]
 
-    respones = {
-        "total_matches": total_matches,
-        team1: int(matches_won_team1),
-        team2: int(matches_won_team2),
-        "draws": int(draws),
-    }
-    return respones
+        matches_won_team1 = temp_df["WinningTeam"].value_counts()[team1]
+        matches_won_team2 = temp_df["WinningTeam"].value_counts()[team2]
+
+        draws = total_matches - (matches_won_team1 + matches_won_team2)
+
+        response = {
+            "total_matches": str(total_matches),
+            team1: str(matches_won_team1),
+            team2: str(matches_won_team2),
+            "draws": str(draws),
+        }
+
+        return response
+    else:
+        return {"message": "invalid team name"}
